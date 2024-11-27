208b2916daa5dbefe605befe3475ed35e57aeab8
JeongHyeon Jo <abeul25@naver.com>
2024-11-27 13:47:18 +0900

    feat: friends -> point

--- lib/features/point/presentation/screens/point_screen.dart
+++ lib/features/point/presentation/screens/point_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class FriendsScreen extends StatelessWidget {
-  const FriendsScreen({super.key});
+class PointScreen extends StatelessWidget {
+  const PointScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('친구'),
+        child: Text('포인트'),
       ),
     );
   }
