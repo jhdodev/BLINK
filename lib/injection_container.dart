5fd9cbd45f24344c5d280c8946a375cdc6175354
jhdodev <jhdo.dev@gmail.com>
2024-11-28 11:35:14 +0900

    feat: 동영상 스트리밍 기능 추가(임시)

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
+import 'features/video/presentation/blocs/video/video_bloc.dart';
 import 'features/navigation/presentation/bloc/navigation_bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
   // Bloc
+  sl.registerFactory(() => VideoBloc());
   sl.registerFactory(() => NavigationBloc());
 }
