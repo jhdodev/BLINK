91e688d0818159892d9ce3f74eb82db350a8408b
JeongHyeon Jo <abeul25@naver.com>
2024-12-01 18:20:57 +0900

    fix: nickname 못불러오는 버그 수정 중

--- lib/features/profile/presentation/blocs/profile_bloc/profile_bloc.dart
+++ lib/features/profile/presentation/blocs/profile_bloc/profile_bloc.dart
@@ -3,25 +3,30 @@ import 'package:equatable/equatable.dart';
 import 'package:blink/features/user/domain/entities/user_entity.dart';
 import 'package:blink/features/video/data/models/video_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:blink/features/user/domain/repositories/user_repository.dart';
+import 'package:blink/features/user/domain/repositories/auth_repository.dart';
 import 'package:blink/features/video/domain/repositories/video_repository.dart';
 
 part 'profile_event.dart';
 part 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  final UserRepository userRepository;
+  final AuthRepository authRepository;
   final VideoRepository videoRepository;
 
   ProfileBloc({
-    required this.userRepository,
+    required this.authRepository,
     required this.videoRepository,
   }) : super(ProfileInitial()) {
     on<LoadProfile>((event, emit) async {
       emit(ProfileLoading());
       try {
-        final userModel = await userRepository.getUserById(event.userId);
+        final userModel = await authRepository.getUserDataWithUserId(event.userId);
         final videos = await videoRepository.getVideosByUserId(event.userId);
+
+        if (userModel == null) {
+          throw Exception("UserModel이 null입니다. 유효한 사용자 ID를 전달했는지 확인하세요.");
+        }
+
         emit(ProfileLoaded(
           user: userModel.toEntity(),
           videos: videos,
