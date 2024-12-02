5c3297b00481372992c06b2d4e24ec45819dd58d
[APP1] 정원교 <dnjsry159@naver.com>
2024-12-02 15:53:30 +0900

    upload preview 구현

--- lib/features/upload/presentation/screens/upload_screen.dart
+++ lib/features/upload/presentation/screens/upload_screen.dart
@@ -1,9 +1,11 @@
+import 'package:blink/core/routes/app_router.dart';
 import 'package:blink/features/upload/presentation/blocs/camera/camera_bloc.dart';
 import 'package:blink/features/upload/presentation/blocs/camera/camera_event.dart';
 import 'package:blink/features/upload/presentation/blocs/camera/camera_state.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class UploadScreen extends StatelessWidget {
   const UploadScreen({super.key});
@@ -11,42 +13,77 @@ class UploadScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => CameraBloc()..add(InitializeCamera()),
-      child: BlocBuilder<CameraBloc, CameraState>(
-        builder: (context, state) {
-          return Scaffold(
-            body: SafeArea(
-              child: Stack(
-                children: [
-                  if (state is CameraInitialized || state is CameraRecording)
-                    CameraPreview((state as dynamic).controller),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: _buildRecordButton(context, state),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: FloatingActionButton(
-                          onPressed:(){
+      create: (context) =>
+      CameraBloc()
+        ..add(InitializeCamera()),
+      child: BlocListener<CameraBloc, CameraState>(
+        listener: (context, state) {
+          if (state is VideoSelected) {
+            context.push("/upload_preview", extra: state.video.path);
+          }
 
-                          },
-                          child: const Icon(Icons.photo)),
-                    ),
-                  ),
-                  if (state is CameraError)
-                    Center(
-                      child: Text(state.message),
-                    ),
-                ],
-              ),
-            ),
-          );
+          if (state is CameraError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text("에러...")),
+            );
+          }
         },
+        child: BlocBuilder<CameraBloc, CameraState>(
+          builder: (context, state) {
+            return Scaffold(
+              body: SafeArea(
+                child: Stack(
+                  children: [
+                    if (state is CameraInitialized || state is CameraRecording)
+                      CameraPreview((state as dynamic).controller),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: _buildRecordButton(context, state),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: FloatingActionButton(
+                            onPressed: () {
+                              context.read<CameraBloc>().add(
+                                  PickVideoFromGallery());
+                            },
+                            child: const Icon(Icons.photo)),
+                      ),
+                    ),
+                    if (state is CameraError)
+                      Center(
+                        child: Text(state.message),
+                      ),
+                    Positioned(
+                      left: 16,
+                      top: 16,
+                      child: GestureDetector(
+                        onTap: () => context.pop(),
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.5),
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Icon(
+                            Icons.close,
+                            color: Colors.white,
+                            size: 24,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
